05a3daffe4e200d61c7661e320d6b93b9891468b
neiljain56 <neiljain56@gmail.com>
2019-05-05 16:40:15 -0400

    Python server

--- ed/pythonServer/try.py
+++ ed/pythonServer/try.py
@@ -1,9 +1,13 @@
 import sys
 
 from flask import Flask, render_template, request, redirect, Response
+from flask_cors import CORS, cross_origin
+
 import random, json
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/encrypt": {"origins": "http://localhost:3000"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
@@ -13,6 +17,14 @@ def index():
 @app.route('/hello')
 def hello():
 	return "Hello World"
+
+@app.route('/encrypt')
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def encrypt():
+	baseString = request.args.get('str') # is no data sent this will return null
+	print('string received is ', baseString)
+	return baseString
+	#return '''Woohoo'''
 
 if __name__ == '__main__':
 	# run!
--- ed/src/components/requests.jsx
+++ ed/src/components/requests.jsx
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import axios from 'axios';
+
 class Requests extends Component {
 
     constructor(){
         super();
-       
+        this.state = {
+            encryptedString: 'ff'
+        }
+        this.getPythonHello = this.getPythonHello.bind(this);
 }
    
     getPythonHello() {
+        console.log("Function called ")
+        axios.get('http://localhost:5000/encrypt?str=ENCRYPT-ME33CORS')
+            .then(response => this.setState({encryptedString : response.data}))
+            .then(response => console.log(response.data))
+            .catch(response => console.log("Can’t access  response. Blocked by browser?", response))
 
-       $.ajax({
-            url: "../pythonServer/try.py",
-            success: function(response){
-                console.log(response)
-            }
-       });
-   
+    //    $.ajax({
+    //         url: "../pythonServer/try.py",
+    //         success: function(response){
+    //             console.log(response)
+    //         }
+    //    });
+    console.log("Function called ENDED ")
     }
 
     
@@ -26,8 +36,7 @@ class Requests extends Component {
 
             <div>
                 <button id="theButton" onClick={this.getPythonHello}>Submit</button>
-
-
+                    <p> eNCRYPTED String is {this.state.encryptedString}</p>
             </div>
          );
     }
